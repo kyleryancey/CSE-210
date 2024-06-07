58d509a71e45734ef089f4fd853dc49d28b132e1
Kyler Yancey <yanceyk21@gmail.com>
2024-06-07 13:42:35 -0600

    Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Create a base "Assignment" object
+        Assignment A1 = new Assignment("Kyler Yancey", "Multiplication");
+        Console.WriteLine(A1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment A2 = new MathAssignment("Karlee Yancey", "Fractions", "7.3", "8-19");
+        Console.WriteLine(A2.GetSummary());
+        Console.WriteLine(A2.GetHomeworkList());
+
+        WritingAssignment A3 = new WritingAssignment("Jake Thueson", "European History", "The Causes of World War II");
+        Console.WriteLine(A3.GetSummary());
+        Console.WriteLine(A3.GetWritingInformation());
     }
 }
